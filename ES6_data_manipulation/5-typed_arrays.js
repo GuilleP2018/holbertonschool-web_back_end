ff5dda923849bf96ae429a7ab67a36adf5887ebb
GuilleP2018 <gpereyo@gmail.com>
2024-01-29 13:12:29 -0400

    5 complete and eslint fix

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer, 0);
 
-  if (position > length -1) {
+  if (position > length - 1) {
     throw Error('Position outside range');
   }
 
